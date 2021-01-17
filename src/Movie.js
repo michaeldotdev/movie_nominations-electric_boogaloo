9b312a4d91e5d1c405fc8eaff55199b1851aa0ef
Michael Su <michaelsu.jobs@gmail.com>
2021-01-17 10:51:25 -0800

    Update and add Movie Component

---
+++ src/Movie.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Movie = () => {
+  return (
+    <div className="movieDetails">
+      <h3 className="title">Title</h3>
+      <p className="year">2021</p>
+      <div className="nominationButton">
+        <button>Nomination</button>
+      </div>
+    </div>
+  )
+}
+
+export default Movie;
